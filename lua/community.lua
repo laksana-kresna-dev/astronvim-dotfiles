e57cc0a1363ed6dba1952baeafefeff5aa66c3b2
AgungKresna-dev <174786763+AgungKresna-dev@users.noreply.github.com>
2026-09-21 20:42:28 +0800

    feat: add cmp plugin and implement copilot ai completion engine

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.recipes.ai" },
   -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.dracula-nvim" },
   { import = "astrocommunity.pack.rust" },
